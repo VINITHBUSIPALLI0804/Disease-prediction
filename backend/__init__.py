f35f7743425977c8414dfbafba4f139802dd008c
vinith busipalli <vinithbusipalli@gmail.com>
2025-10-04 22:30:16 +0530

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,13 +1,9 @@
 from flask import Flask
-
 def create_app():
     app = Flask(__name__)
-
     # Register Blueprints
     from backend.routes.disease_routes import disease_bp
     app.register_blueprint(disease_bp)
-
     from backend.routes.scalability_routes import scalability_bp
     app.register_blueprint(scalability_bp)
-
     return app
